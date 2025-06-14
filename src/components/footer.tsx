5ebb40d531025fd3a8ff93eb75c5b31a2e9be2d3
David F <davidfawzy02@gmail.com>
2025-06-13 21:08:59 -0400

    Portfolio link now opens in new tab

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,13 @@ const Footer = () => {
   return (
     <div className="fixed left-0 bottom-0 h-min w-screen flex justify-center gap-8 p-2 text-xs bg-on-surface border-t-2 border-border">
       <div>
-        Developed By <Link className="font-bold underline" href={process.env.NEXT_PUBLIC_PORTFOLIO_URL}>David Fawzy</Link>
+        Developed By <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold underline"
+          href={process.env.NEXT_PUBLIC_PORTFOLIO_URL}>
+          David Fawzy
+        </Link>
       </div>
       <div className="flex gap-2 text-xs">
         Powered by <Image src="/TMDBLogo.svg" alt="TMDB Logo" width={72} height={32}/>
